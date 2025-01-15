d1ea073cf9f6b394ef370f7533964037b4f29f51
PamelaDahlberg <pamela.a.dahlberg@gmail.com>
2025-01-14 20:08:30 -0700

    drills for chapter 2

--- programming principals and practice/hello_world.cpp
+++ programming principals and practice/hello_world.cpp
@@ -1,5 +1,19 @@
 #include "std_lib_facilities.h"
+// In case std_lib_facilities.h does not exists
+/*
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+*/
 
 int main(){
-    cout << "Hello World!\n";    
+    cout << "Hello World!\n";
+    cout << "Here we go!\n";
+    keep_window_open();
+
+    return 0;;
 }
